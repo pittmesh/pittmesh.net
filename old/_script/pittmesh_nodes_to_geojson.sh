9fa10c710f5746ed43ba49816123ce3f0fe3484a
Colin Dean <git@cad.cx>
2019-04-16 21:13:14 -0400

    Drops "old" directory

--- old/_script/pittmesh_nodes_to_geojson.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -z "$(command -v jq)" ]]; then
-  2>& echo "jq is not available and must be. Install it somehow."
-  2>& echo "If you are seeing this on heroku, ensure that the jq buildpack is installed. See the README."
-  exit 1
-fi
-
-cat "$1" | tail -n +2 | sed '$d' | jq  -s '.[] | { type: "FeatureCollection", features: [ .[] | {type: "Feature", id: .name, properties: { address, hood, status, device_count }, geometry: { type: "Point", coordinates: [ .lon, .lat] } } ] }'
