3bac6fff33da485c3baee181978b004493c666b4
Colin Dean <git@cad.cx>
2019-04-16 21:33:23 -0400

    Addresses shellcheck warnings in geojson generator

--- scripts/pittmesh_nodes_to_geojson.sh
+++ scripts/pittmesh_nodes_to_geojson.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ -z "$(command -v jq)" ]]; then
+  # shellcheck disable=SC2238
   2>& echo "jq is not available and must be. Install it somehow."
+  # shellcheck disable=SC2238
   2>& echo "If you are seeing this on heroku, ensure that the jq buildpack is installed. See the README."
   exit 1
 fi
 
-cat "$1" | tail -n +2 | sed '$d' | jq  -s '.[] | { type: "FeatureCollection", features: [ .[] | {type: "Feature", id: .name, properties: { address, hood, status, device_count }, geometry: { type: "Point", coordinates: [ .lon, .lat] } } ] }'
+tail -n +2 < "${1}" | sed '$d' | jq  -s '.[] | { type: "FeatureCollection", features: [ .[] | {type: "Feature", id: .name, properties: { address, hood, status, device_count }, geometry: { type: "Point", coordinates: [ .lon, .lat] } } ] }'
